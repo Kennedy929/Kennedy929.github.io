b75bcede6b5f13b12c894eaec4fece3918528af3
IsaacMendivil <95067172+Kennedy929@users.noreply.github.com>
2024-03-08 20:18:17 -0700

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,12 @@
+function cambiarColor(){
+    variable_album = document.getElementById("album");
+    console.log(variable_album);
+    variable_album.style.backgroundColor = "green";
+}
+
+function cambiarTamano(){
+    variable_imagen = document.getElementsByClassName("imagen");
+    console.log(variable_imagen);
+    variable_imagen:starset.style.width = "1000px";
+    variable_imagen:starset.style.height = "1000px";
+}
